06247e3e267946f220484cd0e3353f36a974cc58
santiagohirsch <shirsch@itba.edu.ar>
2024-01-30 13:50:20 -0300

    Mutation Bug Exercise

---
+++ Module 2 - Working with State/Complex State/Mutation Bug Exercise/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function App() {
+  const [colors, setColors] = React.useState([
+    '#FFD500',
+    '#FF0040',
+  ]);
+
+  const colorStops = colors.join(', ');
+  const backgroundImage = `linear-gradient(${colorStops})`;
+
+  return (
+    <>
+      <div
+        className="gradient-preview"
+        style={{
+          backgroundImage,
+        }}
+      />
+
+      <form>
+        {colors.map((color, index) => {
+          const colorId = `color-${index}`;
+
+          return (
+            <div
+              key={colorId}
+              className="color-row"
+            >
+              <label htmlFor={colorId}>
+                Color {index + 1}:
+              </label>
+              <input
+                id={colorId}
+                type="color"
+                value={color}
+                onChange={(event) => {
+                  let newColors = [...colors];
+                  newColors[index] =
+                    event.target.value;
+
+                  setColors(newColors);
+                }}
+              />
+            </div>
+          );
+        })}
+      </form>
+    </>
+  );
+}
+
+export default App;
