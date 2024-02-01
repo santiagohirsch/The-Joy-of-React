1c6201699082cb0f42efd0a99db9c0ac183b2dc2
santiagohirsch <shirsch@itba.edu.ar>
2024-02-01 13:41:01 -0300

    The useMemo Hook Examples

---
+++ Module 3 - React Hooks/Memoization/The useMemo Hook/Example 1/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import format from 'date-fns/format';
+
+import useTime from './use-time';
+
+function App() {
+  const [selectedNum, setSelectedNum] = React.useState(100);
+  const time = useTime();
+  
+  const allPrimes = React.useMemo(() => {
+    const result = [];
+    
+    for (let counter = 2; counter < selectedNum; counter++) {
+      if (isPrime(counter)) {
+        result.push(counter);
+      }
+    }
+    
+    return result;
+  }, [selectedNum]);
+  
+  return (
+    <>
+      <p className="clock">
+        {format(time, 'hh:mm:ss a')}
+      </p>
+      <form>
+        <label htmlFor="num">Your number:</label>
+        <input
+          id="num"
+          type="number"
+          value={selectedNum}
+          onChange={(event) => {
+            // To prevent computers from exploding,
+            // we'll max out at 100k
+            let num = Math.min(100_000, Number(event.target.value));
+            
+            setSelectedNum(num);
+          }}
+        />
+      </form>
+      <p>
+        There are {allPrimes.length} prime(s) between 1 and {selectedNum}:
+        {' '}
+        <span className="prime-list">
+          {allPrimes.join(', ')}
+        </span>
+      </p>
+    </>
+  );
+}
+
+function isPrime(n){
+  const max = Math.ceil(Math.sqrt(n));
+  
+  if (n === 2) {
+    return true;
+  }
+  
+  for (let counter = 2; counter <= max; counter++) {
+    if (n % counter === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export default App;
