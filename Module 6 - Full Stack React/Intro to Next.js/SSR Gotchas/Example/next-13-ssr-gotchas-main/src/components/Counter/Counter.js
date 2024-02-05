b4ac520ef02abcb5f92716877b5a3b2050f27c6c
santiagohirsch <shirsch@itba.edu.ar>
2024-02-05 14:48:03 -0300

    SSR Gotchas Example

---
+++ Module 6 - Full Stack React/Intro to Next.js/SSR Gotchas/Example/next-13-ssr-gotchas-main/src/components/Counter/Counter.js
@@ -0,0 +1,31 @@
+'use client';
+import React from 'react';
+import Spinner from '../Spinner'
+
+function Counter() {
+  const [count, setCount] = React.useState(null);
+
+  React.useEffect(() => {
+    const savedValue = window.localStorage.getItem('saved-count');
+
+    setCount(savedValue ? Number(savedValue) : 0)
+  }, []);
+
+  React.useEffect(() => {
+    if (typeof count === 'number') {
+      window.localStorage.setItem('saved-count', count);
+    }
+  }, [count]);
+
+  return (
+    <button
+      className="count-btn"
+      onClick={() => setCount(count + 1)}
+    >
+      Count:{' '}
+      {typeof count === 'number' ? count : <Spinner />}
+    </button>
+  );
+}
+
+export default Counter;
