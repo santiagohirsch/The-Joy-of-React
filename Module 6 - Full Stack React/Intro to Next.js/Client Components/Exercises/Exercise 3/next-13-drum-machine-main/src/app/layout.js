7016eab01fa6287ba418f856ee9fcf233510857a
santiagohirsch <shirsch@itba.edu.ar>
2024-02-05 13:51:23 -0300

    Client Components Exercises

---
+++ Module 6 - Full Stack React/Intro to Next.js/Client Components/Exercises/Exercise 3/next-13-drum-machine-main/src/app/layout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+import './styles.css';
+
+import SoundEnabledProvider from '../components/SoundEnabledProvider';
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+      <SoundEnabledProvider>
+          <Header />
+          {children}
+          <footer>
+            <img
+              src="/ie-badge.gif"
+              width={100}
+            />
+            <span>Thanks for visiting!</span>
+          </footer>
+        </SoundEnabledProvider>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
---
+++ Module 6 - Full Stack React/Intro to Next.js/Client Components/Exercises/Exercise 3/next-13-drum-machine-main/src/components/Header/Header.js
@@ -0,0 +1,43 @@
+'use client';
+import React from 'react';
+import { Volume2, VolumeX } from 'react-feather';
+
+import VisuallyHidden from '../VisuallyHidden';
+import MaxWidthWrapper from '../MaxWidthWrapper';
+import { useSoundEnabled } from '../SoundEnabledProvider';
+import styles from './Header.module.css';
+
+function Header() {
+  const id = React.useId();
+
+  const { soundEnabled, setSoundEnabled } =
+    useSoundEnabled();
+
+  return (
+    <header className={styles.wrapper}>
+      <MaxWidthWrapper
+        className={styles.innerWrapper}
+      >
+        <a href="/">Kool Website</a>
+        <button
+          onClick={() => {
+            setSoundEnabled(!soundEnabled);
+          }}
+        >
+          {soundEnabled ? (
+            <Volume2 />
+          ) : (
+            <VolumeX />
+          )}
+          <VisuallyHidden>
+            {soundEnabled
+              ? 'Disable sound effects'
+              : 'Enable sound effects'}
+          </VisuallyHidden>
+        </button>
+      </MaxWidthWrapper>
+    </header>
+  );
+}
+
+export default Header;
