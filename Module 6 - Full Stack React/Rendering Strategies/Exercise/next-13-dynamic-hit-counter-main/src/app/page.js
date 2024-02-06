3865c27e011b668aaf5e335c6fd900a5daaee03a
santiagohirsch <shirsch@itba.edu.ar>
2024-02-06 10:27:02 -0300

    Rendering Strategies Exercise

---
+++ Module 6 - Full Stack React/Rendering Strategies/Exercise/next-13-dynamic-hit-counter-main/src/app/page.js
@@ -0,0 +1,32 @@
+// src/app/page.js
+import React from 'react';
+import {
+  readFile,
+  writeFile,
+} from '../helpers/file-helpers';
+
+const DATABASE_PATH = '/src/database.json';
+
+export const dynamic = 'force-dynamic';
+
+function Home() {
+  let { hits } = JSON.parse(
+    readFile(DATABASE_PATH)
+  );
+
+  hits += 1;
+
+  writeFile(
+    DATABASE_PATH,
+    JSON.stringify({ hits })
+  );
+
+  return (
+    <main>
+      <h1>Welcome!</h1>
+      <p>You are visitor number {hits}.</p>
+    </main>
+  );
+}
+
+export default Home;
