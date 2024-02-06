c6b4bcb8f8d3300fa3e866c7314701069fa3e2a0
santiagohirsch <shirsch@itba.edu.ar>
2024-02-06 13:57:31 -0300

    Working With Groups Examples

---
+++ Bonus Module - Layout Animations with Framer Motion/Shared Layout/Working With Groups/Examples/Example 1/WidgetProcessor.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { LayoutGroup, motion } from 'framer-motion';
+import { ChevronDown, ChevronUp } from 'react-feather';
+import range from 'lodash.range';
+
+import VisuallyHidden from './VisuallyHidden';
+
+function WidgetProcessor({ total }) {
+  const id = React.useId();
+
+  const [
+    numOfProcessedWidgets,
+    setNumOfProcessedWidgets,
+  ] = React.useState(0);
+
+  const numOfUnprocessedWidgets =
+    total - numOfProcessedWidgets;
+
+  function handleProcessWidget() {
+    if (numOfProcessedWidgets < total) {
+      setNumOfProcessedWidgets(
+        numOfProcessedWidgets + 4
+      );
+    }
+  }
+
+  function handleRevertWidget() {
+    if (numOfProcessedWidgets > 0) {
+      setNumOfProcessedWidgets(
+        numOfProcessedWidgets - 4
+      );
+    }
+  }
+
+  return (
+    <LayoutGroup>
+      <div className="wrapper">
+        <div className="inbox">
+          {range(numOfUnprocessedWidgets).map(
+            (itemNum) => {
+              const layoutId = `${id}-${itemNum}`;
+              return (
+                <motion.div
+                  layoutId={layoutId}
+                  key={layoutId}
+                  className="widget"
+                />
+              );
+            }
+          )}
+        </div>
+
+        <div className="actions">
+          <button onClick={handleProcessWidget}>
+            <VisuallyHidden>
+              Process widget
+            </VisuallyHidden>
+            <ChevronDown />
+          </button>
+          <button onClick={handleRevertWidget}>
+            <ChevronUp />
+            <VisuallyHidden>
+              Revert widget
+            </VisuallyHidden>
+          </button>
+        </div>
+
+        <div className="outbox">
+          {range(numOfUnprocessedWidgets, total).map(
+            (itemNum) => {
+              const layoutId = `${id}-${itemNum}`;
+
+              return (
+                <motion.div
+                  layoutId={layoutId}
+                  key={layoutId}
+                  className="widget"
+                />
+              );
+            }
+          )}
+        </div>
+      </div>
+    </LayoutGroup>
+  );
+}
+
+export default WidgetProcessor;
