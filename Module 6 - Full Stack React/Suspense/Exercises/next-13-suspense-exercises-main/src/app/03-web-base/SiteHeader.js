c47af0bdffe866725f626f59e61411a405679116
santiagohirsch <shirsch@itba.edu.ar>
2024-02-06 11:46:50 -0300

    Suspense Exercises

---
+++ Module 6 - Full Stack React/Suspense/Exercises/next-13-suspense-exercises-main/src/app/03-web-base/SiteHeader.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+
+import { getNavLinks } from "@/helpers/web-base-helpers";
+
+function SiteHeader() {
+  return (
+    <header className="site-header">
+      <Link href="" className="logo">
+        WebBase
+      </Link>
+      <React.Suspense>
+        <HeaderNavLinks />
+      </React.Suspense>
+    </header>
+  );
+}
+
+async function HeaderNavLinks() {
+  let navLinks = await getNavLinks();
+
+  // Only show the first 4 links in the header.
+  navLinks = navLinks.slice(0, 4);
+  return (
+    <nav>
+      <ol className="header-nav-links">
+        {navLinks.map(({ slug, label, href, type }) => (
+          <li key={slug}>
+            <Link href={href} className={`header-nav-link ${type}`}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
+
+export default SiteHeader;
